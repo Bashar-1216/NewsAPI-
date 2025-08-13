5d678106d77a91c4a1607778576d38c5fe3390d5
Bashar Almuntaser <basharalmuntaser2@gmail.com>
2025-08-13 18:55:36 +0300

    Update main.py

--- news_aggregator_backend/src/main.py
+++ news_aggregator_backend/src/main.py
@@ -64,7 +64,6 @@ def serve_static_files(path):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # استخدام PORT من البيئة
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=False)
 
 
